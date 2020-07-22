aeb2f596543e1d6635732a0aef2ff5242ba08ef8
Leo joy <leojoy20@gmail.com>
2020-07-22 15:52:00 +0530

    initial commit

--- react-js accelerator/src/assets/styles/theme/theme-provider.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import { ThemeProvider as OriginalThemeProvider } from 'styled-components';
-import { themeConfig } from './config';
-
-export const ThemeProvider = props => {
-  return <OriginalThemeProvider theme={themeConfig}>{React.Children.only(props.children)}</OriginalThemeProvider>;
-};
